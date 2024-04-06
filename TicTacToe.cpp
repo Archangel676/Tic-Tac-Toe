ceb27d6e3650e620a450ac92b633cd54c1f21d41
Maya <mvenega9@my.chemeketa.edu>
2024-04-06 00:57:28 +0000

    Apr 5, 2024, 5:57 PM

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -14,10 +14,7 @@ TicTacToe::TicTacToe()
 
   for (int i = 0; i < row; i++) {
     for (int j = 0; j < col; j++) {
-      if (j < 2) {
-        board[i][j] = '-';
-      } else
-        board[i][j] = '-\n';
+      board[i][j] = '-';
     }
   }
 }
